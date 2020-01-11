311b8dad3f9ab1473133516aec9413542b969a08
albin-jose <albinavirappatt@gmail.com>
2020-01-11 22:39:09 +0530

    added quiz section and subjects

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"strings"
 )
 
 func main() {
-	var name, subject, v string
+	var name, subject, v, ans string
 	var flag int
 	fmt.Println("Welcome to QUIZOG")
 	fmt.Printf("\nPlease Enter your name: ")
@@ -15,11 +19,12 @@ func main() {
 
 	stream := [3]string{"maths", "science", "gk"}
 
-	//score := 0
+	score := 0
 
 	for {
 		fmt.Printf("\nSelect quiz subject:  ")
 		fmt.Scanln(&subject)
+		subject = strings.ToLower(subject)
 
 		for _, v = range stream {
 			if strings.Compare(subject, v) == 0 {
@@ -34,4 +39,33 @@ func main() {
 			fmt.Println("\nYou had entered a wrong choice.Please choose Maths, Science or GK")
 		}
 	}
+
+	filename := subject + ".csv"
+
+	csvFile, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	csvReader := csv.NewReader(csvFile)
+
+	for {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf(row[0] + " : ")
+		fmt.Scanln(&ans)
+		if strings.Compare(ans, row[1]) == 0 {
+			score += 1
+		}
+	}
+
+	fmt.Println("\nThe total score is ", score)
+
 }
